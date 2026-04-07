2b2cde30f73912b2d9361cf7826f2ea6a1ad4f6e
AravindR-K <aravind05.rk@gmail.com>
2026-04-07 16:45:10 +0530

    Adding central admin inorder to host the quiz

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,9 +1,29 @@
 const mongoose = require('mongoose');
 
+const User = require('./models/User');
+const bcrypt = require('bcrypt');
+
+const createAdmin = async () => {
+  const adminExists = await User.findOne({ email: 'admin@test.com' });
+
+  if (!adminExists) {
+    const hashedPassword = await bcrypt.hash('admin123', 10);
+
+    await User.create({
+      name: 'Admin',
+      email: 'admin@test.com',
+      password: hashedPassword,
+      role: 'admin'
+    });
+
+    console.log('Default admin created');
+  }
+};
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await createAdmin();
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
